a7aab7eb88a11cccc0855739b7800814699e28b6
gaoge <314013991@qq.com>
2024-05-10 20:47:56 +0800

    feat: version 1.0.1

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { VantResolver } from '@vant/auto-import-resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/Project-Showcase',
   build: {
     outDir: 'docs'
   },
